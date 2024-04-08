8514474f2af74ad6cd3169cd81be38d040d60f46
llamsa01 <SamuelLlamzon@gmail.com>
2024-04-08 00:35:52 -0500

    Beginning of Senior Project

---
+++ src/Composer.java
@@ -0,0 +1,8 @@
+public class Composer {
+    //Samuel Llamzon
+    //Senior Project: MUS-490
+    //Experiments in Computer Composition
+    public static void main(String [] args){
+        
+    }
+}
