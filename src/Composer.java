19cbbfc9d8727f0d10ae0f044ca689eedb55f676
llamsa01 <SamuelLlamzon@gmail.com>
2024-04-17 12:17:29 -0500

    RandomGenerator

--- src/Composer.java
+++ src/Composer.java
@@ -1,22 +1,44 @@
 //import java.math.*;
 import java.util.*;
-
+import java.io.*;
 public class Composer {
     //Samuel Llamzon
     //Senior Project: MUS-490
     //Experiments in Computer Composition
     public static void main(String [] args){
         Neume basicPhrase = new Neume();
-        for(int i=0; i<10; i++){
+        for(int i=0; i<100; i++){
+            //candidate
             basicPhrase.addNote(randomNewNote());
         }
-        System.out.println(basicPhrase);
+        System.out.println(basicPhrase.getTranslatedNoteSequence());
+        try{
+            String filename = "rawMusicTextOutput-" +System.currentTimeMillis()+ ".txt";
+            File output = new File(filename);
+            output.createNewFile();
+
+            FileWriter composerScribe = new FileWriter(filename);
+            composerScribe.write("\\version \"2.24.3\"\r\n\r\n{\r\n\t");
+            composerScribe.write(basicPhrase.getTranslatedNoteSequence());
+            composerScribe.write("\r\n}");
+            /*composerScribe.write("\\version \"2.24.3\"\r\n\r\n{\r\n\t");
+            composerScribe.write(basicPhrase.getTranslatedNoteSequence());
+            composerScribe.write("\r\n}");*/
+            composerScribe.close();
+
+            Runtime activeRuntime = Runtime.getRuntime();
+            Process generateLilypondPDF = activeRuntime.exec("lilypond " + filename);
+            generateLilypondPDF.destroy();
+        }
+        catch (IOException exception){
+            
+        }
     }
     //Methods
     public static String toNotes(List<Note> rawSequence){
         String translatedSequence = "";
         for(int i = 0; i < rawSequence.size(); i++){
-            translatedSequence += toNote(rawSequence.get(i).getRawNote()) + " ";
+            translatedSequence += toNote(rawSequence.get(i).getRawNote()) + "\' ";
         }
         return translatedSequence;
     }
